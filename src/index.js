6add4e6cd01576f0efc60c61f0a8f6696339b07e
Ivan Drozdovskiy <vania110297@gmail.com>
2020-10-02 17:19:31 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0) return 0
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0) return 0
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0) return 0
+  let sum = 0
+  array.forEach(element => {
+    sum += element
+  });
+  return sum / array.length;
 }
